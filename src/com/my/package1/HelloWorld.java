6242c6a416b69c41768d6e2b0ea5b8470d782bd8
dabing <1430956666@qq.com>
2021-06-29 18:45:17 +0800

    第四次提交，又一次修改了输出的内容

--- src/com/my/package1/HelloWorld.java
+++ src/com/my/package1/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main (String[] args){
         System.out.println("Hello World!");
         System.out.println("HI IDEA!");
-        System.out.println("fan a ,hai lai");
+        System.out.println("hello!");
     }
 }
