db3ae89410184f5be994ad0c1dc21473f9019341
dabing <1430956666@qq.com>
2021-06-29 16:59:31 +0800

    第二次提交，修改了输出的内容

--- src/com/my/package1/HelloWorld.java
+++ src/com/my/package1/HelloWorld.java
@@ -3,5 +3,6 @@ package com.my.package1;
 public class HelloWorld {
     public static void main (String[] args){
         System.out.println("Hello World!");
+        System.out.println("HI IDEA!");
     }
 }
